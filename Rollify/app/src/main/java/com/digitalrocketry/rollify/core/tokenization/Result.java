a8f586292b66eb569e01b22c348858ce3072cf09
David Aaron Suddjian <aaron@digitalrocketry.com>
2015-09-02 19:08:19 -0700

    Added Result
    holds result of an expression evaluation

---
+++ Rollify/app/src/main/java/com/digitalrocketry/rollify/core/tokenization/Result.java
@@ -0,0 +1,16 @@
+package com.digitalrocketry.rollify.core.tokenization;
+
+/**
+ * Created by David Aaron Suddjian on 9/2/2015.
+ */
+public class Result {
+    private long result;
+
+    public Result(long value) {
+        this.result = value;
+    }
+
+    public long getResult() {
+        return result;
+    }
+}
