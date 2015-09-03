9462bbeeb5461b4725f74d1eb81daec4033d5297
David Aaron Suddjian <aaron@digitalrocketry.com>
2015-09-02 19:06:45 -0700

    Add Utils

---
+++ Rollify/app/src/main/java/com/digitalrocketry/rollify/core/tokenization/Utils.java
@@ -0,0 +1,9 @@
+package com.digitalrocketry.rollify.core.tokenization;
+
+/**
+ * Created by David Aaron Suddjian on 9/2/2015.
+ */
+public class Utils {
+    public static final long MAX_DIE_COUNT = 1000000;
+    public static final long MAX_DIE_TYPE = 1000000000;
+}
