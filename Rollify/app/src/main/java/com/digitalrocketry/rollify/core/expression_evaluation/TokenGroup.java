d7189fe87e3369c48f1c1cd4f01755d941da4ee4
David Aaron Suddjian <aaron@digitalrocketry.com>
2015-09-04 11:13:00 -0700

    Added operate logic to TokenGroup

--- Rollify/app/src/main/java/com/digitalrocketry/rollify/core/expression_evaluation/TokenGroup.java
+++ Rollify/app/src/main/java/com/digitalrocketry/rollify/core/expression_evaluation/TokenGroup.java
@@ -7,16 +7,27 @@ import java.util.Stack;
  * Created by David Aaron Suddjian on 9/1/2015.
  */
 public class TokenGroup extends Token {
-    private Token iterations;
+    private Token iterationToken;
     private List<Token> contents;
 
     public TokenGroup(Token iterations, List<Token> contents) {
-        this.iterations = iterations;
+        this.iterationToken = iterations;
         this.contents = contents;
     }
 
     @Override
     public void operate(Stack<Long> stack) {
+        Stack<Long> temp = new Stack<>();
+        iterationToken.operate(temp);
+        if (temp.size() != 1) throw new InvalidExpressionException();
+        long iterations = temp.pop();
+        boolean negative = iterations < 0;
+        iterations = Math.abs(iterations);
+        long result = 0;
+        for (int i = 0; i < iterations; i++) {
+            result += new Evaluator().evaluate(contents).getResult();
+        }
+        stack.push(result);
     }
 
     @Override
@@ -27,7 +38,7 @@ public class TokenGroup extends Token {
     @Override
     public String toString() {
         StringBuilder steve = new StringBuilder();
-        steve.append(iterations.toString());
+        steve.append(iterationToken.toString());
         for (Token t : contents) {
             steve.append(" ");
             steve.append(t.toString());
