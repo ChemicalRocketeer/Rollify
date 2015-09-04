87b325f0367db51fa67ca36195d0d127b90ff6b3
David Aaron Suddjian <aaron@digitalrocketry.com>
2015-09-04 10:55:40 -0700

    Added dice rolling operation

--- Rollify/app/src/main/java/com/digitalrocketry/rollify/core/expression_evaluation/DieToken.java
+++ Rollify/app/src/main/java/com/digitalrocketry/rollify/core/expression_evaluation/DieToken.java
@@ -21,7 +21,31 @@ public class DieToken extends Token {
 
     @Override
     public void operate(Stack<Long> stack) {
-
+        if (dieType < 1 || dieType > Utils.MAX_DIE_TYPE) throw new InvalidExpressionException("invalid die");
+        Stack<Long> temp = new Stack<Long>();
+        dieCount.operate(temp);
+        if (temp.size() != 1) throw new InvalidExpressionException();
+        long count = temp.pop();
+        boolean negative = false;
+        // if count is negative, we will calculate it as if it were positive and then negate the result.
+        // i.e. -3d4 is treated as -(3d4)
+        if (count < 0) {
+            negative = true;
+            count = -count;
+        }
+        if (count > Utils.MAX_DIE_COUNT) throw new InvalidExpressionException("too many dice");
+        long[] results = new long[(int) count];
+        for (int i = 0; i < results.length; i++) {
+            results[i] = Utils.RAND.nextLong() % dieType + 1;
+        }
+        long total = 0;
+        for (long die : results) {
+            total += die;
+        }
+        if (negative) {
+            total = -total;
+        }
+        stack.push(total);
     }
 
     @Override
