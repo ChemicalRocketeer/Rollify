986487358ea2ca7886b14d66bb5c4bd5af526d80
David Aaron Suddjian <aaron@digitalrocketry.com>
2015-09-09 00:01:18 -0700

    Fixed ParenTokenizer overscan
    Since I combined the openParen and closeParen tokenizers, they are both
    using the same StringScanner, so they can't just read stuff all over the
    place.

--- Rollify/app/src/main/java/com/digitalrocketry/rollify/core/expression_evaluation/ParenTokenizer.java
+++ Rollify/app/src/main/java/com/digitalrocketry/rollify/core/expression_evaluation/ParenTokenizer.java
@@ -8,7 +8,8 @@ import java.util.List;
 public class ParenTokenizer implements Tokenizer {
     @Override
     public boolean tryTokenize(TokenizationContext context, StringScanner sc) {
-        if (sc.next() == '(') {
+        if (sc.peek() == '(') {
+            sc.next();
             List<Token> contents = new TokenizationContext(context, true).tokenize();
             Token multiplier;
             if (context.lastTokenWasnumber()) {
@@ -20,7 +21,8 @@ public class ParenTokenizer implements Tokenizer {
             }
             context.commitToken(new TokenGroup(multiplier, contents));
             return true;
-        } else if (context.isInParentheses() && sc.next() == ')') {
+        } else if (context.isInParentheses() && sc.peek() == ')') {
+            sc.next();
             context.finish();
             return true;
         } else {
