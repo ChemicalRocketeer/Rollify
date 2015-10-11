0af6c375f5616fc12b44e7af01bca2bb3574fd01
David Aaron Suddjian <aasuddjian@gmail.com>
2015-10-11 14:34:35 -0600

    Added full set of calculator buttons and style
    Almost all calculator buttons add some text to the editor, so they are
    all pretty much implemented in the same way. Different kinds of buttons
    have separate methods that they call, however, in case I want to add
    logging or something later specific to each button group.
    
    Styled the buttons with a basic solid background color for now.
    
    I got rid of the fragments because they weren't really contributing
    anything, and it's easier to edit everything in one place.

--- Rollify/app/src/main/java/com/digitalrocketry/rollify/MainActivity.java
+++ Rollify/app/src/main/java/com/digitalrocketry/rollify/MainActivity.java
@@ -2,48 +2,61 @@ package com.digitalrocketry.rollify;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
 
 import com.digitalrocketry.rollify.tasks.EvaluationTask;
 
-import java.util.Dictionary;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements EvaluationTask.Listener {
 
     CalculatorDisplayFragment calcDisplay;
-    Map<Integer, String> numpadMap;
+    Map<Integer, String> entryButtonMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        numpadMap = new HashMap<>();
-        numpadMap.put(R.id.calcNumpad0, "0");
-        numpadMap.put(R.id.calcNumpad1, "1");
-        numpadMap.put(R.id.calcNumpad2, "2");
-        numpadMap.put(R.id.calcNumpad3, "3");
-        numpadMap.put(R.id.calcNumpad4, "4");
-        numpadMap.put(R.id.calcNumpad5, "5");
-        numpadMap.put(R.id.calcNumpad6, "6");
-        numpadMap.put(R.id.calcNumpad7, "7");
-        numpadMap.put(R.id.calcNumpad8, "8");
-        numpadMap.put(R.id.calcNumpad9, "9");
-        numpadMap.put(R.id.calcNumpadOpenParen, "(");
-        numpadMap.put(R.id.calcNumpadCloseParen, ")");
-        numpadMap.put(R.id.calcOperatorAdd, "+");
-        numpadMap.put(R.id.calcOperatorSub, "-");
-        numpadMap.put(R.id.calcOperatorMul, "*");
-        numpadMap.put(R.id.calcOperatorDiv, "/");
+        entryButtonMap = new HashMap<>();
+        entryButtonMap.put(R.id.calcButton0, "0");
+        entryButtonMap.put(R.id.calcButton1, "1");
+        entryButtonMap.put(R.id.calcButton4, "4");
+        entryButtonMap.put(R.id.calcButton2, "2");
+        entryButtonMap.put(R.id.calcButton3, "3");
+        entryButtonMap.put(R.id.calcButton5, "5");
+        entryButtonMap.put(R.id.calcButton6, "6");
+        entryButtonMap.put(R.id.calcButton7, "7");
+        entryButtonMap.put(R.id.calcButton8, "8");
+        entryButtonMap.put(R.id.calcButton9, "9");
+        entryButtonMap.put(R.id.calcButtonOpenParen, "(");
+        entryButtonMap.put(R.id.calcButtonCloseParen, ")");
+        entryButtonMap.put(R.id.calcButtonAdd, "+");
+        entryButtonMap.put(R.id.calcButtonSub, "-");
+        entryButtonMap.put(R.id.calcButtonMul, "*");
+        entryButtonMap.put(R.id.calcButtonDiv, "/");
+        entryButtonMap.put(R.id.calcButtonAdvD, "d");
+        entryButtonMap.put(R.id.calcButtonAdvH, "h");
+        entryButtonMap.put(R.id.calcButtonAdvL, "l");
+        entryButtonMap.put(R.id.calcButtonAdvF, "df");
+        entryButtonMap.put(R.id.calcButtonD2, "d2");
+        entryButtonMap.put(R.id.calcButtonD4, "d4");
+        entryButtonMap.put(R.id.calcButtonD6, "d6");
+        entryButtonMap.put(R.id.calcButtonD8, "d8");
+        entryButtonMap.put(R.id.calcButtonD10, "d10");
+        entryButtonMap.put(R.id.calcButtonD12, "d12");
+        entryButtonMap.put(R.id.calcButtonD20, "d20");
+        entryButtonMap.put(R.id.calcButtonD100, "d100");
         calcDisplay = (CalculatorDisplayFragment) getFragmentManager().findFragmentById(R.id.calcDisplayFragment);
     }
 
-    public void evaluateAndDisplay(View v) {
+    public void evaluateExpressionAndDisplay() {
         String expression = calcDisplay.getEditorText();
         new EvaluationTask().addListener(this).execute(expression);
+    }
+
+    public void onCalcEqualsButtonPressed(View view) {
+        evaluateExpressionAndDisplay();
     }
 
     @Override
@@ -56,14 +69,26 @@ public class MainActivity extends AppCompatActivity implements EvaluationTask.Li
         calcDisplay.setDisplayText(task.getResult());
     }
 
-    public void onNumpadButtonPressed(View view) {
-        String str = numpadMap.get(view.getId());
+    public void onCalcEntryButtonPressed(View view) {
+        String str = entryButtonMap.get(view.getId());
         if (str != null) {
             calcDisplay.insertTextAtCursor(str);
         }
     }
 
-    public void onOperatorButton(View view) {
-        onNumpadButtonPressed(view);
+    public void onCalcNumpadButtonPressed(View view) {
+        onCalcEntryButtonPressed(view);
+    }
+
+    public void onCalcOperatorButtonPressed(View view) {
+        onCalcEntryButtonPressed(view);
+    }
+
+    public void onCalcAdvancedButtonPressed(View view) {
+        onCalcEntryButtonPressed(view);
+    }
+
+    public void onCalcDButtonPressed(View view) {
+        onCalcEntryButtonPressed(view);
     }
 }
