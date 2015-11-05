63278011a965aec50420dc21509e964660dc5a03
David Aaron Suddjian <aasuddjian@gmail.com>
2015-11-05 15:32:10 -0800

    Add Formula Adapter to populate the formula list

--- Rollify/app/src/main/java/com/digitalrocketry/rollify/FormulaAdapter.java
+++ Rollify/app/src/main/java/com/digitalrocketry/rollify/FormulaAdapter.java
@@ -1,30 +1,54 @@
 package com.digitalrocketry.rollify;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.digitalrocketry.rollify.db.Formula;
 
 /**
  * Adapter for the formula list view
  */
 public class FormulaAdapter extends BaseAdapter {
-    @Override
-    public int getCount() {
-        return 0;
+
+    private Context context;
+    private Formula[] formulas;
+    
+    public FormulaAdapter(Context context, Formula[] formulas) {
+        this.context = context;
+        setFormulas(formulas);
+    }
+
+    public void setFormulas(Formula[] formulas) {
+        this.formulas = formulas;
     }
 
     @Override
-    public Object getItem(int position) {
-        return null;
+    public int getCount() {
+        return formulas.length;
+    }
+
+    @Override
+    public Formula getItem(int position) {
+        return formulas[position];
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return getItem(position).id;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        View v = convertView;
+        if (convertView == null) {
+            v = LayoutInflater.from(context).inflate(R.layout.formula_list_item, null);
+        }
+        String name = getItem(position).name;
+        ((TextView) v.findViewById(R.id.formulaName)).setText(name);
+        return v;
     }
 }
