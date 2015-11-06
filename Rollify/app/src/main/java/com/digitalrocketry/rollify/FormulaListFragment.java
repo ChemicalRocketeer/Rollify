2e14e3495d5a651684d5b8134a5c0b118687e5f3
David Aaron Suddjian <aasuddjian@gmail.com>
2015-11-05 21:40:48 -0800

    Connect FormulaAdapter to the formula list

--- Rollify/app/src/main/java/com/digitalrocketry/rollify/FormulaListFragment.java
+++ Rollify/app/src/main/java/com/digitalrocketry/rollify/FormulaListFragment.java
@@ -6,6 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import com.digitalrocketry.rollify.db.Formula;
 
 
 /**
@@ -13,17 +17,26 @@ import android.view.ViewGroup;
  */
 public class FormulaListFragment extends Fragment {
 
+    private FormulaAdapter adapter;
 
     public FormulaListFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_formula_list, container, false);
+        View layout = inflater.inflate(R.layout.fragment_formula_list, container, false);
+        ListView list = (ListView) layout.findViewById(R.id.formulaListView);
+        adapter = new FormulaAdapter(getContext(), new Formula[] {
+                new Formula("Formula 1", "2d6+5d4+2", 0, 5),
+                new Formula("Formula 2", "d4d12", 0, 3),
+                new Formula("Formula 3", "3(2d10)", 0, 1),
+        });
+        adapter.sort(Formula.COMPARE_BY_NAME);
+        list.setAdapter(adapter);
+        return layout;
     }
 
 
