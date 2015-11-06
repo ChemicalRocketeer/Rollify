98a078c93d69aa1471ab69407d95f2773a1a26fb
David Aaron Suddjian <aasuddjian@gmail.com>
2015-11-05 16:59:17 -0800

    Chage FormulaAdapter to extend ArrayAdapter

--- Rollify/app/src/main/java/com/digitalrocketry/rollify/FormulaAdapter.java
+++ Rollify/app/src/main/java/com/digitalrocketry/rollify/FormulaAdapter.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import com.digitalrocketry.rollify.db.Formula;
@@ -12,28 +12,10 @@ import com.digitalrocketry.rollify.db.Formula;
 /**
  * Adapter for the formula list view
  */
-public class FormulaAdapter extends BaseAdapter {
+public class FormulaAdapter extends ArrayAdapter<Formula> {
 
-    private Context context;
-    private Formula[] formulas;
-    
     public FormulaAdapter(Context context, Formula[] formulas) {
-        this.context = context;
-        setFormulas(formulas);
-    }
-
-    public void setFormulas(Formula[] formulas) {
-        this.formulas = formulas;
-    }
-
-    @Override
-    public int getCount() {
-        return formulas.length;
-    }
-
-    @Override
-    public Formula getItem(int position) {
-        return formulas[position];
+        super(context, R.layout.formula_list_item, formulas);
     }
 
     @Override
@@ -45,7 +27,7 @@ public class FormulaAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         View v = convertView;
         if (convertView == null) {
-            v = LayoutInflater.from(context).inflate(R.layout.formula_list_item, null);
+            v = LayoutInflater.from(getContext()).inflate(R.layout.formula_list_item, null);
         }
         String name = getItem(position).name;
         ((TextView) v.findViewById(R.id.formulaName)).setText(name);
