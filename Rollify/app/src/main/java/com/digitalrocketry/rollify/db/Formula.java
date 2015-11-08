9944d02e9ecb911d9d3438bf8b82ace4a1deb7a3
David Aaron Suddjian <aasuddjian@gmail.com>
2015-11-08 00:08:04 -0800

    Add more methods and encapsulate Formula

--- Rollify/app/src/main/java/com/digitalrocketry/rollify/db/Formula.java
+++ Rollify/app/src/main/java/com/digitalrocketry/rollify/db/Formula.java
@@ -1,6 +1,8 @@
 package com.digitalrocketry.rollify.db;
 
 import com.orm.SugarRecord;
+import com.orm.query.Condition;
+import com.orm.query.Select;
 
 import java.util.Comparator;
 
@@ -9,11 +11,10 @@ import java.util.Comparator;
  */
 public class Formula extends SugarRecord<Formula> {
 
-    public long id;
-    public String name;
-    public String expression;
-    public long creationTime;
-    public int uses;
+    private String name;
+    private String expression;
+    private long creationTime;
+    private int uses;
 
     public Formula(String name, String expression, long creationTime, int uses) {
         super();
@@ -29,6 +30,8 @@ public class Formula extends SugarRecord<Formula> {
 
     public Formula() {
         super();
+        this.creationTime = System.currentTimeMillis();
+        this.uses = 0;
     }
 
     private static final long MILLIS_PER_HOUR = 1000 * 60 * 60;
@@ -41,6 +44,34 @@ public class Formula extends SugarRecord<Formula> {
         long time = (System.currentTimeMillis() - creationTime) / MILLIS_PER_HOUR;
         if (time == 0) time = 1; // prevent any potential division by 0.
         return uses / time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getExpression() {
+        return expression;
+    }
+
+    public void setExpression(String expression) {
+        this.expression = expression;
+    }
+
+    public long getCreationTime() {
+        return creationTime;
+    }
+
+    public int getUses() {
+        return uses;
+    }
+
+    public void setUses(int uses) {
+        this.uses = uses;
     }
 
     public static final Comparator<Formula> COMPARE_BY_NAME = new Comparator<Formula>() {
@@ -60,4 +91,12 @@ public class Formula extends SugarRecord<Formula> {
             return Long.compare(a.creationTime, b.creationTime);
         }
     };
+
+    public static Formula findByID(long id) {
+        return Select.from(Formula.class).where(Condition.prop("id").eq(id)).first();
+    }
+
+    public static Formula findByName(String name) {
+        return Select.from(Formula.class).where(Condition.prop("name").eq(name)).first();
+    }
 }
