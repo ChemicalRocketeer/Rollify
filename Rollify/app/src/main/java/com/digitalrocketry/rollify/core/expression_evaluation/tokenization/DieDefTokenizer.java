aeb2cb240d7c00c0d049c48a4eb75f10a66f89d1
David Aaron Suddjian <aasuddjian@gmail.com>
2015-12-05 02:21:46 -0800

    Add df support in DieDefTokenizer
    It basically just pushes a d3 - 2 when it finds a df.

--- Rollify/app/src/main/java/com/digitalrocketry/rollify/core/expression_evaluation/tokenization/DieDefTokenizer.java
+++ Rollify/app/src/main/java/com/digitalrocketry/rollify/core/expression_evaluation/tokenization/DieDefTokenizer.java
@@ -3,7 +3,14 @@ package com.digitalrocketry.rollify.core.expression_evaluation.tokenization;
 import com.digitalrocketry.rollify.core.expression_evaluation.ExpressionUtils;
 import com.digitalrocketry.rollify.core.expression_evaluation.InvalidExpressionException;
 import com.digitalrocketry.rollify.core.expression_evaluation.tokens.DieToken;
+import com.digitalrocketry.rollify.core.expression_evaluation.tokens.IntegerToken;
+import com.digitalrocketry.rollify.core.expression_evaluation.tokens.Operator;
 import com.digitalrocketry.rollify.core.expression_evaluation.tokens.Token;
+import com.digitalrocketry.rollify.core.expression_evaluation.tokens.TokenGroup;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by David Aaron Suddjian on 9/8/2015.
@@ -15,16 +22,29 @@ import com.digitalrocketry.rollify.core.expression_evaluation.tokens.Token;
  * representing how many sides the die has.
  */
 public class DieDefTokenizer implements Tokenizer {
+
+    private static List<Token> fudgeEquation = Arrays.asList(
+            new DieToken(new IntegerToken(1), 3),
+            new IntegerToken(2),
+            Operator.SUB);
+
     @Override
     public boolean tryTokenize(TokenizationContext context, StringScanner sc)
             throws  InvalidExpressionException {
         if (sc.next() == 'd') {
             sc.skipWhitespace();
-            if (!Character.isDigit(sc.peek()))
+            if (sc.hasNextDigit()) {
+                long dieType = sc.nextLong();
+                Token dieCount = ExpressionUtils.findCoefficientToken(context);
+                context.pushToOutput(new DieToken(dieCount, dieType));
+            } else if (sc.hasNext('f')) {
+                // this is a fudge die
+                sc.next();
+                Token dieCount = ExpressionUtils.findCoefficientToken(context);
+                context.pushToOutput(new TokenGroup(dieCount, fudgeEquation));
+            } else {
                 throw new InvalidExpressionException("No die type specified");
-            long dieType = sc.nextLong();
-            Token dieCount = ExpressionUtils.findCoefficientToken(context);
-            context.pushToOutput(new DieToken(dieCount, dieType));
+            }
             return true;
         } else {
             return false;
